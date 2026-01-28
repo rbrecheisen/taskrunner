6504e1bd664265f950744b4cb1b573b4f070dd42
Brecheisen <r.brecheisen@maastrichtuniversity.nl>
2026-01-28 13:20:39 +0100

    Updated installdeps.bat script

--- scripts/installdeps.bat
+++ scripts/installdeps.bat
@@ -14,3 +14,6 @@ call mamba install -c conda-forge ^
     opencv ^
     matplotlib ^
     simpleitk
+
+@rem Installing dependencies using pip
+@rem pip install django django-jinja django-session-security django-guardian django-crispy-forms crispy-bootstrap5 numpy pandas pydicom pillow opencv matplotlib simpleitk
